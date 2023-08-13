19467d0c36206b0fc83c6086200da5a779162189
ApurbaChk <ac872013@student.nitw.ac.in>
2023-08-14 02:52:54 +0530

    Model training with hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -14,8 +14,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-#from src.components.model_trainer import ModelTrainerConfig
-#from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass  #a decorator to use the class
 class DataIngestionConfig:  #saving the inputs
     train_data_path: str=os.path.join('artifacts',"train.csv") #path that is being saved
@@ -60,7 +60,7 @@ if __name__=="__main__":
     data_transformation=DataTransformation()  #call the self.data_transformation_config fn from data_transformation
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    #modeltrainer=ModelTrainer()
-    #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
